43d93b8a4ece61f8503e6c79cd1a81ece2c51049
Nicolas Kauan <166877999+NicolasKauan@users.noreply.github.com>
2026-03-23 21:01:09 -0300

    Criação do controller

---
+++ ex2_lista_javaspringboot/src/main/java/com/nika/ex2_lista_javaspringboot/controllers/ProdutoController.java
@@ -0,0 +1,38 @@
+package com.nika.ex2_lista_javaspringboot.controllers;
+
+import com.nika.ex2_lista_javaspringboot.models.Produto;
+import com.nika.ex2_lista_javaspringboot.services.ProdutoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    private final ProdutoService service;
+
+    public ProdutoController(ProdutoService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Produto criar(@RequestBody Produto produto) {
+        return service.salvar(produto);
+    }
+
+    @GetMapping
+    public List<Produto> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Produto buscar(@PathVariable Long id) {
+        return service.buscar(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
+}
